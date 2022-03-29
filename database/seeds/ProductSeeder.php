ce2cbd46bb44560c47f32d472346cf69bfc99efa
kurniarocki <kurniarocki30@gmail.com>
2022-03-29 21:43:06 +0700

    seeder product

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Helpers\GeneralHelper;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $path = base_path("database\csv\products.csv");
+        $provinces = GeneralHelper::csvToArray($path, "|");
+        \DB::transaction(function() use($provinces) {
+            $collection = collect($provinces);
+            $parts = $collection->chunk(1000);
+            foreach ($parts as $subset) {
+                \DB::table('products')->insert($subset->toArray());
+            }
+        });
+    }
+}
