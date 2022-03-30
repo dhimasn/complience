ebc2aaf4ccc5dbc578f3daec79b9c5ea054db33f
kurniarocki <kurniarocki30@gmail.com>
2022-03-30 09:18:19 +0700

    update new model

---
+++ app/Models/Complience.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Complience extends Model
+{
+    protected $primayKey = 'record_id';
+    public $incrementing = false;
+    protected $table = 'complience';
+}
---
+++ app/Models/Formulir3.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Formulir3 extends Model
+{
+    protected $table = 'formulir_3';
+}
