f39068f4e12b2c5e733b84f6502d7c586abdf5ae
kurniarocki <kurniarocki30@gmail.com>
2022-04-03 12:44:52 +0700

    google maps layers for leaflet

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -102,9 +102,11 @@
 <script>
   var map = L.map('map').setView([-2.000373, 116.526913], 4);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      attribution: '&copy; <a href="https://www.google.com">Google Maps</a> contributors'
+      }).addTo(map);
 
 var data = {
     labels: ["Sesuai","Tidak Sesuai"],
