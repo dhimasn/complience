71c765bec11d848b41c9a4c1eec0d84785e56a99
kurniarocki <kurniarocki30@gmail.com>
2022-04-15 10:32:27 +0700

    fixing login mobile

--- app/Http/Controllers/Api/Mobile/AuthController.php
+++ app/Http/Controllers/Api/Mobile/AuthController.php
@@ -10,12 +10,13 @@ use App\User;
 class AuthController extends Controller
 {
     public function login(Request $request){
-        if(\Auth::attempt(['name' => $request->username, 'password' => $request->password])){ 
+        if(\Auth::attempt(['email' => $request->username, 'password' => $request->password, 'id_user_role' => 2])){ 
             $user = \Auth::user(); 
             $response['success'] = true;
             $response['message'] = "200 OK";
             $response['token'] =  $user->createToken('MyApp')->accessToken; 
             $response['data'] =  array(
+                'id_user' => $user->id,
                 'nama' => $user->name,
                 'jabatan' => $user->role_user->category_user
             );
--- app/User.php
+++ app/User.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use App\Models\LabUji;
-use App\Models\UserRole;
+use App\Models\UserRoleDB;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,7 +41,7 @@ class User extends Authenticatable
     ];
 
     public function role_user(){
-        return $this->belongsTo(UserRole::class, 'id_user_role');
+        return $this->belongsTo(UserRoleDB::class, 'id_user_role');
     }
     public function labUji(){
         return $this->belongsTo(LabUji::class, 'id_lab');
