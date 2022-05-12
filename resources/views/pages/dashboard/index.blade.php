0661a7ddc5c488a0a21538979c9513292eadf9a1
kurniarocki <kurniarocki30@gmail.com>
2022-05-12 12:02:03 +0700

    update chart

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -34,170 +34,168 @@
 @endsection
 @section('contents')
 <div class="row">
-  <div class="col-xl-6 col-md-6">
+  <div class="col-xl-12 col-md-12">
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-12">
+            <form action="" method="GET">
+              <div class="form-group row">
+                <label class="col-sm-2">Peralatan</label>
+                <div class="col-sm-3">
+                  <select name="pelaratan" class="form-control form-complience">
+                    <option value="2">AC</option>
+                    <option disabled value="">Kulkas</option>
+                    <option disabled value="">Kipas Angin</option>
+                    <option disabled value="">Penanak Nasi</option>
+                    <option disabled value="1">Lampu CFL</option>
+                    <option disabled value="">Lampu LED</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-2">Periode</label>
+                <div class="col-sm-9">
+                  <div class="row">
+                    <div class="col-md-1">
+                      <input type="radio" name="periode" value="tahun">
+                    </div>
+                    <div class="col-md-1">
+                      <label class="">Tahun</label>
+                    </div>
+                    <div class="col-md-3">
+                      <input type="number" placeholder="Tahun" name="tahun" class="form-control form-complience">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-1">
+                      <input type="radio" name="periode" value="date">
+                    </div>
+                    <div class="col-md-1">
+                      <label class="">Dari</label>
+                    </div>
+                    <div class="col-md-3">
+                      <input type="date" name="dari" class="form-control form-complience">
+                    </div>
+                    <div class="col-md-1">
+                      <label class="">Hingga</label>
+                    </div>
+                    <div class="col-md-3">
+                      <input type="date" name="hingga" class="form-control form-complience">
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-2"></label>
+                <div class="col-sm-3">
+                  <button class="btn btn-yellow button-comp" submit>
+                    Filter
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-xl-12 col-md-12">
     <div class="card">
       <div class="card-header py-2">
-        Jumlah Produk
+        <a data-toggle="tooltip" data-placement="top" title="Market Insights"><i class="fas fa-info-circle"></i></a>
+        Market Insights
       </div>
       <div class="card-body">
         <div class="row">
-          <div class="col-md-3">
+          <div class="col-md-4">
             <div class="row">
               <div class="col">
-                <h5 class="card-title text-uppercase text-muted mb-0">AC</h5>
+                <h5 class="card-title text-uppercase text-muted mb-0">JUMLAH PERUSAHAAN</h5>
                 <span class="h2 font-weight-bold mb-0">35</span>
               </div>
             </div>
           </div>
-          <div class="col-md-3">
-            <div class="row">
-              <div class="col">
-                <h5 class="card-title text-uppercase text-muted mb-0">Kulkas</h5>
-                <span class="h2 font-weight-bold mb-0">35</span>
-              </div>
-            </div>
+          <div class="col-md-4">
+            <table>
+              <tr>
+                <td align="right"><h5 class="card-title text-uppercase text-muted mb-0">JUMLAH PRODUSEN DALAM NEGERI</h5></td>
+                <td><span class="h2 font-weight-bold mb-0 ml-4">35</span></td>
+              </tr>
+              <tr>
+                <td align="right"><h5 class="card-title text-uppercase text-muted mb-0">JUMLAH UNIT PRODUKSI TERLAPOR</h5></td>
+                <td><span class="h2 font-weight-bold mb-0 ml-4">35</span></td>
+              </tr>
+              <tr>
+                <td align="right"><h5 class="card-title text-uppercase text-muted mb-0">JUMLAH IMPORTIR</h5></td>
+                <td><span class="h2 font-weight-bold mb-0 ml-4">35</span></td>
+              </tr>
+              <tr>
+                <td align="right"><h5 class="card-title text-uppercase text-muted mb-0">JUMLAH UNIT IMPOR TERLAPOR</h5></td>
+                <td><span class="h2 font-weight-bold mb-0 ml-4">35</span></td>
+              </tr>
+            </table>
           </div>
         </div>
       </div>
     </div>
     <div class="card">
+      <div class="card-header py-2">
+        <a data-toggle="tooltip" data-placement="top" title="Compliance Insights"><i class="fas fa-info-circle"></i></a>
+        Compliance Insights
+      </div>
+      <div class="card-body p-1">
+        <div class="row">
+          <div class="col-6">
+            <canvas id="barUjiPetikDate" height="150"></canvas>
+          </div>
+          <div class="col-6">
+            <canvas id="barInspeksiVisualDate" height="150"></canvas>
+            <canvas id="barInspeksiVisual" height="150"></canvas>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{-- <div class="card">
       <div class="card-header py-2">
         Jumlah Produk Inspeksi
       </div>
       <div class="card-body p-1">
         <canvas id="barJumlahProduk" height="160"></canvas>
       </div>
-    </div>
-    <div class="card">
+    </div> --}}
+    {{-- <div class="card">
       <div class="card-header py-2">
         Kepatuhan Uji Petik
       </div>
       <div class="card-body p-1">
         <div class="row">
-          <div class="col-md-12 text-center custom-legend">
-            <div class="legend">
-              <div class="orange"></div>
-              <div>Tidak Sesuai</div>
-            </div>
-            <div class="legend">
-              <div class="blue"></div>
-              <div>Sesuai</div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-6">
-            <canvas id="chartVerifikasiAC" height="300"></canvas>
-          </div>
-          <div class="col-6">
-            <canvas id="chartVerifikasiKulkas" height="300"></canvas>
+          <div class="col-12">
+            <canvas id="chartVerifikasi" height="160"></canvas>
           </div>
         </div>
       </div>
-    </div>
-    <div class="card">
-      <div class="card-header py-2">
-        Dampak Regulasi
-      </div>
-      <div class="card-body p-1">
-        <div class="row">
-          <div class="col-6">
-            <canvas id="barDampakRegulasiEnergi" height="350"></canvas>
+    </div> --}}
+  </div>
+  <div class="col-xl-12 col-md-12">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header py-1">
+            <a data-toggle="tooltip" data-placement="top" title="Peta Sebaran Kegiatan Inspeksi"><i class="fas fa-info-circle"></i></a>
+            Peta Sebaran Kegiatan Inspeksi
           </div>
-          <div class="col-6">
-            <canvas id="barDampakRegulasiEmisi" height="350"></canvas>
-          </div>
+          <div id="map" style="height: 383px"></div>
         </div>
       </div>
     </div>
   </div>
-  <div class="col-xl-6 col-md-6">
-    <div class="row">
-      <div class="col-xl-6 col-md-6">
-        <div class="card card-stats">
-          <!-- Card body -->
-          <div class="card-body">
-            <div class="row">
-              <div class="col">
-                <h5 class="card-title text-uppercase text-muted mb-0">Produsen Dalam Negeri</h5>
-                <span class="h2 font-weight-bold mb-0">35</span>
-              </div>
-              <div class="col-auto">
-                <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
-                  <i class="ni ni-active-40"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="card card-stats">
-          <!-- Card body -->
-          <div class="card-body">
-            <div class="row">
-              <div class="col">
-                <h5 class="card-title text-uppercase text-muted mb-0">Total Produk Diinspeksi</h5>
-                <span class="h2 font-weight-bold mb-0">{{$totalProdukInspeksi}}</span>
-              </div>
-              <div class="col-auto">
-                <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
-                  <i class="ni ni-chart-bar-32"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-xl-6 col-md-6">
-        <div class="card card-stats">
-          <div class="card-body">
-            <div class="row">
-              <div class="col">
-                <h5 class="card-title text-uppercase text-muted mb-0">Jumlah Importir</h5>
-                <span class="h2 font-weight-bold mb-0">-</span>
-              </div>
-              <div class="col-auto">
-                <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
-                  <i class="ni ni-active-40"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="card card-stats">
-          <div class="card-body">
-            <div class="row">
-              <div class="col">
-                <h5 class="card-title text-uppercase text-muted mb-0">Jumlah Perusahaan</h5>
-                <span class="h2 font-weight-bold mb-0">-</span>
-              </div>
-              <div class="col-auto">
-                <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
-                  <i class="ni ni-active-40"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="col-xl-12 col-md-12">
     <div class="row">
       <div class="col-md-12">
         <div class="card">
           <div class="card-header py-2">
-            Kepatuhan Inspeksi Visual
-          </div>
-          <div class="card-body p-1">
-              <canvas id="barInspeksiVisual" height="150"></canvas>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header py-1">
-            Peta Sebaran Inspeksi
-          </div>
-          <div id="map" style="height: 383px"></div>
-        </div>
-        <div class="card">
-          <div class="card-header py-2">
+            <a data-toggle="tooltip" data-placement="top" title="Status Uji Petik"><i class="fas fa-info-circle"></i></a>
             Status Uji Petik
           </div>
           <div class="table-responsive">
@@ -242,6 +240,28 @@
                 @endforeach
               </tbody>
             </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-xl-12 col-md-12">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header py-2">
+            <a data-toggle="tooltip" data-placement="top" title="Dampak Regulasi"><i class="fas fa-info-circle"></i></a>
+             Dampak Regulasi
+          </div>
+          <div class="card-body p-1">
+            <div class="row">
+              <div class="col-6">
+                <canvas id="barDampakRegulasiEnergi" height="190"></canvas>
+              </div>
+              <div class="col-6">
+                <canvas id="barDampakRegulasiEmisi" height="190"></canvas>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -340,57 +360,28 @@
     // end map
 
     //chart Kepatuhan Uji petik
-    var data = {
-        labels: ["Sesuai","Tidak Sesuai"],
-        datasets: [
-            {
-                data: [{{$ketidaksesuai['sesuai']}}, {{$ketidaksesuai['tidak_sesuai']}}],
-                backgroundColor: ["#7ba7b0","#f39800"]
-            }]
-    };
-    var chartVerifikasiAC = document.getElementById("chartVerifikasiAC");
-    new Chart(chartVerifikasiAC, {
-        type: 'pie',
-        data: data,
-        options: {
-          pieceLabel: {
-            render: 'value' //show values
-          },
-          legend: {
-            display: false,
-          },
-          title: {
-            display: true,
-            text: 'AC'
-          }
-        }
-    });
-    var data = {
-        labels: ["Sesuai","Tidak Sesuai"],
-        datasets: [
-            {
-                data: [0,0],
-                backgroundColor: ["#7ba7b0","#f39800"]
-            }]
-    };
-
-    var chartVerifikasiKulkas = document.getElementById("chartVerifikasiKulkas");
-    new Chart(chartVerifikasiKulkas, {
-        type: 'pie',
-        data: data,
-        options: {
-          pieceLabel: {
-            render: 'value' //show values
-          },
-          legend: {
-            display: false,
-          },
-          title: {
-            display: true,
-            text: 'Kulkas'
-          }
-        }
-    });
+    // var data = {
+    //     labels: ["Sesuai","Tidak Sesuai"],
+    //     datasets: [
+    //         {
+    //             data: [{{$ketidaksesuai['sesuai']}}, {{$ketidaksesuai['tidak_sesuai']}}],
+    //             backgroundColor: ["#7ba7b0","#f39800"]
+    //         }]
+    // };
+    // var chartVerifikasi = document.getElementById("chartVerifikasi");
+    // new Chart(chartVerifikasi, {
+    //     type: 'pie',
+    //     data: data,
+    //     options: {
+    //       pieceLabel: {
+    //         render: 'value' //show values
+    //       },
+    //       title: {
+    //         display: true,
+    //         text: 'AC'
+    //       }
+    //     }
+    // });
     // END Kepatuhan Uji petik
 
     // CHART Kepatuhan Inspeksi Visual
@@ -412,76 +403,51 @@
                 {{$kepatuhan['4']}},
                 {{$kepatuhan['5']}}
               ],
-              backgroundColor: "#7ba7b0"
-            },
-            {
-              label: 'Kulkas',
-              data: [0,0,0,0,0],
-              backgroundColor: "#f39800"
-            },
+              backgroundColor: ["#36a2eb","#ff6384","#4bc0c0","#ff9f40","#f39800"]
+            }
         ]
     };
     var barInspeksiVisual = document.getElementById("barInspeksiVisual");
     new Chart(barInspeksiVisual, {
-        type: 'horizontalBar',
+        type: 'pie',
         data: data,
         options: {
           pieceLabel: {
             render: 'value' //show values
           },
-          elements: {
-            bar: {
-              borderWidth: 2,
-            }
-          },
           responsive: true,
           legend: {
-            position: 'top',
-          },
-          scales: {
-            xAxes: [{
-                ticks: {
-                    min: 0,
-                    max: 2,
-                    callback: function(value) {
-                      return (value / 2 * 100).toFixed(0) + '%';
-                    }
-                },
-                scaleLabel: {
-                    display: true,
-                    labelString: "Percentage"
-                }
-            }]
-          },
+            position: 'right',
+          }
         }
     });
     // END Kepatuhan Inspeksi Visual
 
     // CHART Produk
-    var yearNow = new Date().getFullYear();
-    var data = {
-        labels: ["AC","Kulkas"],
-        datasets: [
-            {
-              label: 'Produk Inspeksi '+yearNow,
-              data: [7, 0],
-              backgroundColor: "#f7dfa0"
-            },
-        ]
-    };
-    var barJumlahProduk = document.getElementById("barJumlahProduk");
-    new Chart(barJumlahProduk, {
-        type: 'horizontalBar',
-        data: data,
-        options: {
-          elements: {
-            bar: {
-              borderWidth: 2,
-            }
-          },
-          responsive: true,
-        }
-    });
+    // var yearNow = new Date().getFullYear();
+    // var data = {
+    //     labels: ["AC","Kulkas"],
+    //     datasets: [
+    //         {
+    //           label: 'Produk Inspeksi '+yearNow,
+    //           data: [7, 0],
+    //           backgroundColor: "#f7dfa0"
+    //         },
+    //     ]
+    // };
+    // var barJumlahProduk = document.getElementById("barJumlahProduk");
+    // new Chart(barJumlahProduk, {
+    //     type: 'horizontalBar',
+    //     data: data,
+    //     options: {
+    //       elements: {
+    //         bar: {
+    //           borderWidth: 2,
+    //         }
+    //       },
+    //       responsive: true,
+    //     }
+    // });
     // END Produk
 
     // CHART Dampak Regulasi (Penghematan energi)
@@ -565,6 +531,88 @@
         }
     });
     // END Produk
+
+    // CHART barUjiPetikDate
+    var data = {
+        labels: ["MT C02"],
+        datasets: [
+            {
+              label: 'AC',
+              data: [10],
+              backgroundColor: "#7ba7b0"
+            },
+            {
+              label: 'Kulkas',
+              data: [8],
+              backgroundColor: "#f39800"
+            }
+        ]
+    };
+    var barUjiPetikDate = document.getElementById("barUjiPetikDate");
+    new Chart(barUjiPetikDate, {
+        type: 'bar',
+        data: data,
+        options: {
+          scales: {
+            xAxes: [{
+                stacked: true
+            }],
+            yAxes: [{
+                stacked: true
+            }]
+          },
+          responsive: true,
+          legend: {
+            display: false,
+          },
+          title: {
+            display: true,
+            text: 'Uji Petik'
+          }
+        }
+    });
+    // END barUjiPetikDate
+
+    // CHART barInspeksiVisualDate
+    var data = {
+        labels: ["MT C02"],
+        datasets: [
+            {
+              label: 'AC',
+              data: [10],
+              backgroundColor: "#7ba7b0"
+            },
+            {
+              label: 'Kulkas',
+              data: [8],
+              backgroundColor: "#f39800"
+            }
+        ]
+    };
+    var barInspeksiVisualDate = document.getElementById("barInspeksiVisualDate");
+    new Chart(barInspeksiVisualDate, {
+        type: 'bar',
+        data: data,
+        options: {
+          scales: {
+            xAxes: [{
+                stacked: true
+            }],
+            yAxes: [{
+                stacked: true
+            }]
+          },
+          responsive: true,
+          legend: {
+            display: false,
+          },
+          title: {
+            display: true,
+            text: 'Inspeksi Visual'
+          }
+        }
+    });
+    // END barInspeksiVisualDate
 
     $(document).on("click", ".linkDetail", function(){
       var name = $(this).data('name');
